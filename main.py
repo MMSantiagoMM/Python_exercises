250269699be2485c2978266b2f41bae41a1ba08a
MMSantiagoMM <santiagomosquera2@gmail.com>
2024-04-06 18:02:52 -0500

    Deleting unnecesary elements

--- main.py
+++ main.py
@@ -8,21 +8,8 @@ print(squareRoot)
 
 
 names1 = ["Santiago","Carla","Maria","Rosario","Diego"]
-
-name = 'Santiago'
 vowels = "aeiou"
-otherName = [];
 vowelsList=[];
-
-print(name[0])
-
-
-for i in range(len(name)):
-    if name[i]== vowels[0] or name[i]==vowels[1] or name[i]==vowels[2] or name[i]==vowels[3] or name[i]==vowels[4]:
-        otherName.append(name[i]);
-
-print(otherName)
-
 
 def myFunction(names):
     vowelsList = []
@@ -37,21 +24,5 @@ def myFunction(names):
 myNewListVowels = list(map(myFunction,names1))
 
 print(myNewListVowels)
-
-
-
-
-
-
-
-
-myList = ["Santiago","Cristina"]
-
-myList.append(["Rosario"])
-
-myList.insert(3,["k","F"])
-
-
-print(myList)
 
 
